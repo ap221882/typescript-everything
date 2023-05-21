6f44bcfc5fbcbd5d4fc4dd6e6c7999081fe45124
ajay <ap221882@gmail.com>
2023-05-21 19:55:22 +0530

    structural vs nominal

---
+++ 02-TypeCategories/structuralNominal/index.ts
@@ -0,0 +1,4 @@
+//> TypeChecking means checking type equivalence
+//? 1. In return types if return type is provided, 2. while invoking or assigning
+
+//> TS supports structural not nominal type system
