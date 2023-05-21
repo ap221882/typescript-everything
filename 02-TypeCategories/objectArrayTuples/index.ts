11f5529ef4d1ffebdac7eee418734b66e3708598
ajay <ap221882@gmail.com>
2023-05-21 13:57:21 +0530

    objects

---
+++ 02-TypeCategories/objectArrayTuples/index.ts
@@ -0,0 +1,7 @@
+//> Objects -
+//> 1.  what are the properties available
+//> 2. what are the types of the properties available
+
+let cat: { name: string; species: string; sound: string; age: number };
+
+cat.age = 10;
