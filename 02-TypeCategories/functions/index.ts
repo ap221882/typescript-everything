fb6e50116cf8f9f927acd6257290c1c4ccda8cc6
ajay <ap221882@gmail.com>
2023-05-21 13:57:12 +0530

    functions

---
+++ 02-TypeCategories/functions/index.ts
@@ -0,0 +1,5 @@
+//~> tsc comes with installing typescript
+
+//~! Either you can define types where functions are defined or where they are being used
+
+//* either or usage light up(giving explicit return type) - or your usages at 15+ places light up
