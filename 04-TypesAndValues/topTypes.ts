158e963a11d00270949d1a70cf4b9695cdce7fa5
ajay <ap221882@gmail.com>
2023-05-23 20:06:25 +0530

    top types

---
+++ 04-TypesAndValues/topTypes.ts
@@ -0,0 +1,11 @@
+//~# any and unknown
+
+/**
+ ** Types - the set of values that can be allowed on a variable 
+
+ ** Top Types- any, unknown
+
+ ** any - needed when the type can be anything
+
+ ** unknown - can’t be assigned without a type guard  - can be used for data layer(values recieved at runtime) - and can be type guarded
+ */
