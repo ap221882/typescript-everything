a8f7f42246aa93b823588b6accfae4fd76ca875c
ajay <ap221882@gmail.com>
2023-06-02 10:08:33 +0530

    conditional types

---
+++ 07-ConditionalTypes/conditionalTypes.ts
@@ -0,0 +1,7 @@
+//~> Conditional Types - like ternary operator for types
+//* some utility types that are based on these conditional types - Extract and Exclude
+
+//! Syntax - condition ? expressionIfTrue : expressionIfFalse
+//! all these are type expressions
+
+//# T extends <sometype> means T should be **atleast** the sometype by inference or be extended from it etc. -- like a >= comparison
